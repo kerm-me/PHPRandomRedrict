1e10c92a58711dedea0be92d476802113d0d5ac6
WoL <45914900+WoLeo-Z@users.noreply.github.com>
2022-04-28 10:35:49 +0800

    WoL fixed bug of index.php
    你的index好像bug住了，直接报错
    这是我的ver 你看看

--- index.php
+++ index.php
@@ -1,37 +1,19 @@
 <?php
-// https://qqvps.com/d/804
-/* 必须修改links.txt才可使用，例如，按照默认links.txt，访问yourdomain?origin=virmach，会随机跳转virmach官网和google中一个。 */
-$fs = fopen("links.txt","r");
+$filename = "links.txt";
+if(!file_exists($filename)){
+    die('文件不存在');
+}
 
-$lastorigin = '';
-$diction = array();
-
+$links = [];
+$fs = fopen($filename, "r");
 while(!feof($fs)){
     $line=trim(fgets($fs));
     if($line!=''){
-      // https://www.php.cn/php-weizijiaocheng-327042.html
-      if(substr($line,0,1)=='#'){
-        $lastorigin = substr($line,1);
-        $diction[$lastorigin]=[];
-      }
-      else{
-        array_push($diction[$lastorigin], $line);
-      }
+        array_push($links, $line);
     }
 }
 
-// https://www.w3school.com.cn/php/php_get.asp
-$origin = $_GET['origin'];
-
-$all = $diction[$origin];
-
-// https://blog.csdn.net/haibo0668/article/details/91976123
-$url=$all[array_rand($all,1)];
-
-// 注意这里要用双引号
-// header用于跳转：https://www.php.cn/php-weizijiaocheng-394656.html
-header("Location:$url");
-
-
+//从数组随机获取链接
+$link = $links[array_rand($links)];
+die(header("Location: $link"));
 ?>
-
